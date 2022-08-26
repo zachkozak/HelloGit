1b3465e87a0ca5c565d10020cf69cfec277baf24
mjanak4 <mjanak4@10.64.101.48>
2022-08-26 13:02:10 -0500

    added my name and major to the output

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author zachkozak
+ * Hack 01 Java: Git edition
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World! -Zach Kozak");
+		System.out.println("(Computer Science Major)");
+		System.out.println("Michael Janak");
+		System.out.println("Computer Science as well!");
+		
+	}
+}
