f82cb50fc89d9013372d4dc791dee378e138f7bb
mjanak4 <mjanak4@huskers.unl.edu>
2022-08-26 13:05:40 -0500

    fixed missing my name line

--- src/Hello.java
+++ src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello World! -Zach Kozak");
 		System.out.println("(Computer Science Major)");
-		System.out.println("Michael Janak");
+		System.out.println("Michael Janak!");
 		System.out.println("Computer Science as well!");
 		
 	}
